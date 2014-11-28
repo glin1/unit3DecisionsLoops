4ab77c6df0bf563450d3388f0bb8c0c606c5f1ce
glin1 <samlin203@yahoo.com>
2014-11-28 15:17:56 -0600

    Update GameOfLife.java

--- GameOfLifeLab/GameOfLife.java
+++ GameOfLifeLab/GameOfLife.java
@@ -16,11 +16,11 @@ public class GameOfLife
 {
     // the world comprised of the grid that displays the graphics for the game
     private ActorWorld world;
-    
-    // the game board will have 5 rows and 5 columns
+
+    // the game board will have 7 rows and 7 columns
     private final int ROWS = 7;
     private final int COLS = 7;
-    
+
     // constants for the location of the three cells initially alive
     private final int X1 = 2, Y1 = 3;
     private final int X2 = 3, Y2 = 2;
@@ -28,29 +28,28 @@ public class GameOfLife
     private final int X4 = 3, Y4 = 4;
     private final int X5 = 4, Y5 = 2;
     private final int X6 = 4, Y6 = 4;
-    
+
     /**
      * Default constructor for objects of class GameOfLife
      * 
-     * @post    the game will be initialized and populated with the initial state of cells
-     * 
+     * @post    the game will be initialized and populated with the initial state of cells 
      */
     public GameOfLife()
     {
         // create the grid, of the specified size, that contains Actors
         BoundedGrid<Actor> grid = new BoundedGrid<Actor>(ROWS, COLS);
-        
+
         // create a world based on the grid
         world = new ActorWorld(grid);
-        
+
         // populate the game
         populateGame();
-        
+
         // display the newly constructed and populated world
         world.show();
-        
+
     }
-    
+
     /**
      * Creates the actors and inserts them into their initial starting positions in the grid
      *
@@ -63,28 +62,28 @@ public class GameOfLife
         // the grid of Actors that maintains the state of the game
         //  (alive cells contains actors; dead cells do not)
         Grid<Actor> grid = world.getGrid();
-        
+
         // create and add rocks (a type of Actor) to the three intial locations
         Flower flower1 = new Flower();
         Location loc1 = new Location(X1, Y1);
         grid.put(loc1, flower1);
-        
+
         Flower flower2 = new Flower();
         Location loc2 = new Location(X2, Y2);
         grid.put(loc2, flower2);
-        
+
         Flower flower3 = new Flower();
         Location loc3 = new Location(X3, Y3);
         grid.put(loc3, flower3);
-        
+
         Flower flower4 = new Flower();
         Location loc4 = new Location(X4, Y4);
         grid.put(loc4, flower4);
-        
+
         Flower flower5 = new Flower();
         Location loc5 = new Location(X5, Y5);
         grid.put(loc5, flower5);
-        
+
         Flower flower6 = new Flower();
         Location loc6 = new Location(X6, Y6);
         grid.put(loc6, flower6);
@@ -100,134 +99,115 @@ public class GameOfLife
      */
     public void createNextGeneration()
     {
-        /** You will need to read the documentation for the World, Grid, and Location classes
-         *      in order to implement the Game of Life algorithm and leverage the GridWorld framework.
-         */
-        
+     /** Testing the 8 adjacent positions of a space to determine how many dead cells (represented by empty spaces) there is around a particular cell. The variable "count" represents
+      *  the number of dead cells. This variable will be reset to 0 every time the loop runs.If count=5, then a new cell is drawn to the new graph. And if count=6 and the space possesses a alive cell,
+      *  then draw the cell to the new graph. The variable "a" is made to ensure all if statements have a condition to run.
+      */
+
         // create the grid, of the specified size, that contains Actors
         Grid<Actor> grid = world.getGrid();
-        
+
         //create the grid, of the specified size, that contains Actors
         BoundedGrid<Actor> grid2 = new BoundedGrid<Actor>(ROWS, COLS);
-        
+        int a=0;
         int count =0;
-        for (int x=1; x < 6; x++)
-            {
-                for (int y=1; y< 6; y++)
-                {   
-                    count=0;
-                    Location loc3 = new Location(x, y);
-                    Actor actor2 = world.getGrid().get(loc3);
-                    if  ( true)
+        for (int x=1; x < ROWS-1; x++)
+        {
+            for (int y=1; y< COLS-1; y++)
+            {   
+                count=0;
+                Location loc3 = new Location(x, y);
+                if  ( a==0)
+                {
+                    Location loc4 = new Location(x-1, y-1);
+                    if (grid.get(loc4) == null)
                     {
-                        Location loc4 = new Location(x-1, y-1);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
+                        count++;
                     }
+                }
+
+                if  ( a==0)
+                {
+                    Location loc4 = new Location(x-1, y);
+                    if ( grid.get(loc4) == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if  ( a==0)
+                {
+                    Location loc4 = new Location(x-1, y+1);
+                    if (grid.get(loc4) == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if  ( a==0)
+                {
+                    Location loc4 = new Location(x, y-1);
+                    if (grid.get(loc4) == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if  (a==0)
+                {
+                    Location loc4 = new Location(x, y+1);
+                    if (grid.get(loc4) == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if  ( a==0)
+                {
+                    Location loc4 = new Location(x+1, y-1);
+                    if (grid.get(loc4) == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if  ( a==0)
+                {
+                    Location loc4 = new Location(x+1, y);
+                    if (grid.get(loc4) == null)
+                    {
+                        count++;
+                    }
+                }
+
+                if  ( a==0)
+                {
+                    Location loc4 = new Location(x+1, y+1);
+                    if (grid.get(loc4) == null)
+                    {
+                        count++;
+                    }
+                }
                     
-                    if  ( true)
-                    {
-                        Location loc4 = new Location(x-1, y);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
-                    }
-                    
-                    if  ( true)
-                    {
-                        Location loc4 = new Location(x-1, y+1);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
-                    }
-                    
-                    if  ( true)
-                    {
-                        Location loc4 = new Location(x, y-1);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
-                    }
-                    
-                    if  ( true)
-                    {
-                        Location loc4 = new Location(x, y+1);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
-                    }
-                    
-                    if  ( true)
-                    {
-                        Location loc4 = new Location(x+1, y-1);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
-                    }
-                    
-                    if  ( true)
-                    {
-                        Location loc4 = new Location(x+1, y);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
-                    }
-                    
-                    if  ( true)
-                    {
-                        Location loc4 = new Location(x+1, y+1);
-                        Actor actor = world.getGrid().get(loc4);
-                        if (actor == null)
-                        {
-                            count++;
-                            
-                        }
-                    }
-        
-                    while (true)
-                    {
-                        if (count==5)
-                        {
-                            Location loc2=new Location (x,y);
-                            Flower flower2= new Flower();
-                            grid2.put (loc2,flower2);
-                        }
-                        else if (count==6 && actor2 != null)
-                        {
-                            Location loc2=new Location (x,y);
-                            Flower flower2= new Flower();
-                            grid2.put (loc2,flower2);
-                        }
-                    }
-                }   
-            }
-        
-        // display the newly constructed and populated world
-        world.setGrid(grid2);
+                if (count==5)
+                {
+                    Location loc2=new Location (x,y);
+                    Flower flower2= new Flower();
+                    grid2.put (loc2,flower2);
+                }
+                
+                if (count==6 && grid.get(loc3) != null)
+                {
+                    Location loc2=new Location (x,y);
+                    Flower flower2= new Flower();
+                    grid2.put (loc2,flower2);
+                }
+                // display the newly constructed and populated world
+                world.setGrid(grid2);
+            }   
+        }
     }
-    
+
     /**
      * Returns the actor at the specified row and column. Intended to be used for unit testing.
      *
@@ -252,7 +232,7 @@ public class GameOfLife
     {
         return ROWS;
     }
-    
+
     /**
      * Returns the number of columns in the game board
      *
@@ -262,21 +242,21 @@ public class GameOfLife
     {
         return COLS;
     }
-    
-    
+
     /**
      * Creates an instance of this class. Provides convenient execution.
-     *
+     * Creates a while loop to run the createNextgeneration method. 
      */
     public static void main(String[] args) throws InterruptedException
     {
         GameOfLife game = new GameOfLife();
-        while (true)
+        int b=0;
+        while (b==0)
         {
             Thread.sleep(1000);
             game.createNextGeneration();
         }
-    
+
     }
 
 }
